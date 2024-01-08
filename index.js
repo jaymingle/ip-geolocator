0309948a35f4f1778c7cfea39b0a238dd68fb541
Jay Mingle <jkawudi@gmail.com>
2024-01-08 15:59:11 +0000

    Changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function getIPAddress(){
-
+    const getData = fetch("https://ipinfo.io/41.218.196.221/json?token=28b9a5040d38ce")
+        .then(response => console.log(response.json()))
+        .catch(err => console.log(err))
 }
