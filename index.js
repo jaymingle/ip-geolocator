c4cd86484efd9a1399f80da406a60fdeb5456d3e
Jay Mingle <jkawudi@gmail.com>
2024-01-08 16:24:58 +0000

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getIPAddress(){
     const getData = fetch("https://api.ipify.org?format=jsonp&callback=getip")
-        .then(response => console.log(response))
+        .then(response => console.log())
         .catch(err => console.log(err))
 }
