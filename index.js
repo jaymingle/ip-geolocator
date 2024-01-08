fa79fa1c78f9ea46a82d382d21c1a2f95a1c4fae
Jay Mingle <jkawudi@gmail.com>
2024-01-08 16:28:41 +0000

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getIPAddress(){
-    const getData = fetch("https://api.bigdatacloud.net/data/client-ip")
+   fetch("https://api.bigdatacloud.net/data/client-ip")
         .then(response => console.log(response))
         .catch(err => console.log(err))
 }
