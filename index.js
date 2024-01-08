4c06345607e1c8fc86fa9b85ab6918db535e040a
Jay Mingle <jkawudi@gmail.com>
2024-01-08 16:55:38 +0000

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 function getIPAddress(){
-   fetch("https://api.ipify.org?format=jsonp")
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
+    fetch("https://api.ipify.org?format=json")
 }
