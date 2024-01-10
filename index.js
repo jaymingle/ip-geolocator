31713aceafe5c669cd9331dfb7e6fd3ac5d05689
Jay Mingle <jkawudi@gmail.com>
2024-01-10 15:34:56 +0000

    Changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let ip = ""
 
 function getIPAddress(){
-    fetch("https://api.ipify.org?format=json")
-        .then(response => {
-            if(!response.ok){
-                throw new Error("Could not fetch data")
-            }
-            return response.json()
-        })
-        // .then(data => console.log(data))
-        .then(data => ip = data)
-        .catch(err => console.log(err))
-
-    console.log(ip)
+    // fetch("https://api.ipify.org?format=json")
+    //     .then(response => {
+    //         if(!response.ok){
+    //             throw new Error("Could not fetch data")
+    //         }
+    //         return response.json()
+    //     })
+    //     // .then(data => console.log(data))
+    //     .then(data => ip = data)
+    //     .catch(err => console.log(err))
+    //
+    // console.log(ip)
 }
 
 console.log(ip)
